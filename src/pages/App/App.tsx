4df0e2140c2d7ed7c391546dac860c61d54b59b2
Khaled Saidi <khaled.saidi@medisinn.com>
2022-03-10 11:13:28 +0100

    ref(app): use tailwindCSS

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -4,19 +4,33 @@ import { LAST_X_DAYS } from "../../constants";
 import Dashboard from "../Dashboard/Dashboard";
 
 const App: React.FC = () => {
-
   return (
     <div style={{ textAlign: "center" }}>
       <Header>
-        <h2 className="text-3xl font-bold mb-4" style={{ fontFamily: "'Comfortaa', cursive" }}>
+        <h2
+          className="text-3xl font-bold mb-4"
+          style={{ fontFamily: "'Comfortaa', cursive" }}
+        >
           Covid-19 Local Updates - München
         </h2>
-        <h4 style={{ color: '#52525b' }}>Data for the last {LAST_X_DAYS} days</h4>
+        <h4 style={{ color: "#52525b" }}>
+          Data for the last {LAST_X_DAYS} days
+        </h4>
       </Header>
       <Content>
         <Dashboard />
       </Content>
-      <Footer>Khaled Saidi ©{new Date().getFullYear()}</Footer>
+      <Footer>
+        <a
+          href="https://github.com/BangaFlow"
+          rel="noopener noreferrer"
+          target="_blank"
+          className="text-blue-700 hover:text-blue-500 cursor-wait"
+        >
+          Khaled Saidi
+        </a>{" "}
+        ©{new Date().getFullYear()}
+      </Footer>
     </div>
   );
 };
